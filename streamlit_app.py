dc6211bc786c0d9694a6e6bcb1ee84cb77d01086
Harsh Singh <harshking6@gmail.com>
2024-05-19 12:17:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,23 +3,21 @@ import requests
 import json
 
 
-st.set_page_config(page_title="MLops Project",)
-
+st.set_page_config(page_title="MLops Project")
 st.title('Delhi house rent prediction')
 
+st.text("This application is used to predict house rent prediction for the city of Delhi")
+
 # Input features
-bhk  = st.slider('BHK: Number of Bedrooms, Hall, Kitchen (ranges from 1 to 5)',1,5,3)
-sqft = st.slider('Size: Size of the Houses/Apartments/Flats in Square Feet (ranges from 25 sqft to 4000 sqft)',25,4000,1100)
-br   = st.slider('Bathroom: Number of Bathrooms (ranges from 1 to 7)',1,7,2)
+bhk  = st.s('BHK: Number of Bedrooms, Hall, Kitchen',1,5,3)
+sqft = st.slider('Size: Size of the Houses/Apartments/Flats in Square Feet',25,4000,1100)
+br   = st.slider('Bathroom: Number of Bathrooms',1,7,2)
+
+
 
 # Update this URL to point to your deployed Flask API
 url = 'https://flask-test-63qz.onrender.com/predict'
-
-
-st.text('Predicted value:')
-st.text("Rent : Rent of the Houses/Apartments/Flats")
-
-
+st.text("Feature to predict : Rent of the Houses/Apartments/Flats")
 if st.button('Predict'):
     response = requests.post(url, json={'features': [bhk,sqft,br]})
     if response.status_code == 200:
@@ -27,3 +25,9 @@ if st.button('Predict'):
         st.success(f'According to your provided input values, the rent predicted is INR : {prediction}')
     else:
         st.error('Failed to get prediction')
+
+Created by -
+Anupam Dhiman
+Harsh Singh
+Lokesh Arora
+
